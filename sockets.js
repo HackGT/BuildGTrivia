5a38507345b2a83a67015a09756fa648603fe5a7
Matthew Kaufer <mjkaufer@gmail.com>
2019-02-17 19:30:58 -0500

    Questions!

--- sockets.js
+++ sockets.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const generator = require('./questionGenerator')
 
 const port = 3000
 const teams = {
     red: "red",
     blue: "blue"
+}
+const teamScores = {}
+for (var key in teams) {
+    teamScores[teams[key]] = 0
 }
 
 // let's us access client files easily
@@ -24,12 +29,38 @@ console.log("Listening on localhost:" + port)
 io.on('connection', function(socket) {
     // maybe convert this to a weighted random based on the # of team members currently present
     socket.team = (Math.random() > 0.5) ? teams.red : teams.blue
+    socket.hasQuestion = false
+    socket.answer = null
 
     // send this user 'hello'
     socket.emit('hello', {team: socket.team});
 
-    // when the user sends 'world' back, we console log
-    socket.on('world', function(data) {
-        console.log("This user responded with", data)
-    });
+    socket.emit('scoreUpdate', teamScores)
+
+    socket.on('question', function(data) {
+        if (socket.hasQuestion) {
+            return
+        }
+        socket.hasQuestion = true
+        var question = generator()
+        socket.answer = question.answer
+
+        socket.emit('question', question.question)
+    })
+
+    socket.on('answer', function(data) {
+        if (!socket.hasQuestion) {
+            return
+        }
+        socket.hasQuestion = false
+        // console.log(socket.answer, data)
+        var result = socket.answer.toLowerCase() == data.toLowerCase()
+        socket.emit('result', result)
+
+        if (result) {
+            teamScores[socket.team] += 1
+
+            io.emit('scoreUpdate', teamScores)
+        }
+    })
 });
